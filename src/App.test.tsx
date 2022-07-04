f8ae8c833824db796abaa92e5866b89c7e35d32a
Pavel Angelov <pavel@st6.io>
2022-07-04 13:40:30 +0300

    chore: remove unnecessary template (CRA-TS) files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the task title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/DevOcean React Task/i);
+  expect(titleElement).toBeInTheDocument();
 });
